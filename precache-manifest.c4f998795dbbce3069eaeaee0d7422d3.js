1917d03d463a7ca91f166e15a66a48cbc532a215
AlexMndzf <alexmendezfuentes@MacBook-Air-de-Alex.local>
2020-03-28 21:17:18 +0100

    Updates

---
+++ precache-manifest.c4f998795dbbce3069eaeaee0d7422d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c176cdafcea947161028c82334ec8459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a95e3e4446783bc2414",
+    "url": "/static/css/main.a3f0b166.chunk.css"
+  },
+  {
+    "revision": "67c320c8478f08155b0f",
+    "url": "/static/js/2.aab80c77.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aab80c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a95e3e4446783bc2414",
+    "url": "/static/js/main.3e167b04.chunk.js"
+  },
+  {
+    "revision": "fb49e821b26f6bbddfbb",
+    "url": "/static/js/runtime-main.1706e722.js"
+  }
+]);
